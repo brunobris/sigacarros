4324029c1c88b1c8514374bda149c409f986da9d
brunobris <brunobristot@yahoo.com.br>
2016-11-02 18:54:59 -0200

    AlreadyExistsException should extend RuntimeException!

--- src/main/java/br/sigacarros/exceptions/AlreadyExistsException.java
+++ src/main/java/br/sigacarros/exceptions/AlreadyExistsException.java
@@ -1,9 +1,21 @@
 package br.sigacarros.exceptions;
 
 @SuppressWarnings("serial")
-public class AlreadyExistsException extends Exception {
-	public AlreadyExistsException() { super(); }
-	public AlreadyExistsException(String message) { super(message); }
-	public AlreadyExistsException(String message, Throwable cause) { super(message, cause); }
-	public AlreadyExistsException(Throwable cause) { super(cause); }
+public class AlreadyExistsException extends RuntimeException {
+	
+	public AlreadyExistsException() {
+		super(); 
+	}
+	
+	public AlreadyExistsException(String message) {
+		super(message);
+	}
+	
+	public AlreadyExistsException(String message, Throwable cause) {
+		super(message, cause); 
+	}
+	
+	public AlreadyExistsException(Throwable cause) {
+		super(cause);
+	}
 }
